e867e3663ef9e4acf816ce40f6b33a6eea475434
abhishek-iitj <abhi.sah.97@gmail.com>
2017-11-12 17:50:38 +0530

    Adding the chunk grammar

--- file2.py
+++ file2.py
@@ -1,9 +1,12 @@
 import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 def ie_preprocess(document):
     sentences = nltk.sent_tokenize(document)
-    print sentences
+    # print sentences
     sentences = [nltk.word_tokenize(sent) for sent in sentences]
-    print sentences
+    # print 1
+    # print sentences
     sentences = [nltk.pos_tag(sent) for sent in sentences]
     print sentences
     return sentences
@@ -11,16 +14,28 @@ def ie_preprocess(document):
 data=''
 with open('input.txt', 'r') as myfile:
     data=myfile.read().replace('\n', '')
+    # print data
+    # print 'Hi'
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(data)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    # print filtered_sentence
+    data=""
+    for i in range(0, len(filtered_sentence)):
+        data=data+" "+filtered_sentence[i]
+    print data
 
 sentence=ie_preprocess(data)
 #sentence = [("the", "DT"), ("little", "JJ"), ("yellow", "JJ"), ("dog", "NN"), ("barked", "VBD"), ("at", "IN"), ("the", "DT"), ("cat", "NN")]
 grammar = "NP: {<DT>?<JJ>*<NN>}"
-grammar2="LEN: {<$><CD>}"
+grammar2="LEN: {<CD><IN>*<NN>}"
+grammar3='''MES: {<NN><CD>|<CD><NN>|<\$><CD>|<CD><IN>*<NN>|<CC><CD>}
+            COLOR:{<JJ><NN>|<NN><JJ>}'''
 chunked = []
 
-cp = nltk.RegexpParser(grammar)
+cp = nltk.RegexpParser(grammar3)
 for s in sentence:
     result=cp.parse(s)
     chunked.append(result)
     result.draw()
-print chunked
+# print chunked
