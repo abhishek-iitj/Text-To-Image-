91d2deee90e9a3030227e31d8ab5334876b51789
Abhishek Sah <abhishek-iitj>
2018-03-09 19:06:34 +0530

    First Commit. Phase 2

--- file3.py
+++ file3.py
@@ -1,15 +1,12 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-def ie_preprocess(document):
-    sentences = nltk.sent_tokenize(document)
-    # print sentences
-    sentences = [nltk.word_tokenize(sent) for sent in sentences]
-    # print 1
-    # print sentences
-    sentences = [nltk.pos_tag(sent) for sent in sentences]
-    print sentences
-    return sentences
+from nltk.tag import TrigramTagger
+from nltk.corpus import treebank
+
+train_data = treebank.tagged_sents()[:3000]
+print train_data[0]
+from nltk.tag import hmm
 
 data=''
 with open('input6.txt', 'r') as myfile:
@@ -23,8 +20,15 @@ with open('input6.txt', 'r') as myfile:
         data=data+" "+filtered_sentence[i]
     print data
 
+
+def ie_preprocess(data):
+    trainer = hmm.HiddenMarkovModelTrainer()
+    tagger = trainer.train_supervised(train_data)
+    print tagger
+    return tagger.tag(data.split())
+
 sentence=ie_preprocess(data)
-#sentence = [("the", "DT"), ("little", "JJ"), ("yellow", "JJ"), ("dog", "NN"), ("barked", "VBD"), ("at", "IN"), ("the", "DT"), ("cat", "NN")]
+
 grammar = "NP: {<DT>?<JJ>*<NN>}"
 grammar2="LEN: {<CD><IN>*<NN>}"
 grammar3='''MES: {<NN><CD>|<CD><NN>|<\$><CD>|<CD><IN>*<NN>|<CC><CD>|<JJ><CD>}
@@ -36,4 +40,3 @@ for s in sentence:
     result=cp.parse(s)
     chunked.append(result)
     result.draw()
-# print chunked
